07c1f0bbffd399b1d03349b5235fae8d666caa23
Martin Kanovski <mkanovski@gmail.com>
2016-11-16 17:58:03 +0200

    working on homework 1

--- C Sharp Advanced/TestProject/Program.cs
+++ C Sharp Advanced/TestProject/Program.cs
@@ -1,15 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            char order = char.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            if (order == 'a')
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        int input = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            else if (order == 'b')
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j % 2 == 0 || j == 0)
+                    {
+                        for (int i = 0; i < matrix.GetLength(0); i++)
+                        {
+                            int input = int.Parse(Console.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+                        {
+                            int input = int.Parse(Console.ReadLine());
+                        }
+                    }
+                }
+            }
+            else if (order == 'c')
+            {
+                int j = 0;
+                int i = matrix.GetLength(0) - 1;
+                int nextRow = matrix.GetLength(0) - 1;
+
+                while (j <= matrix.GetLength(1) - 1 && i >= 0)
+                {
+                    matrix[i,j] = int.Parse(Console.ReadLine());
+                    if (i == matrix.GetLength(0) - 1)
+                    {
+                        nextRow--;
+                        i = nextRow;
+                        j = 0;
+
+                    }
+                    else
+                    {
+                        i++;
+                        j++;
+                    }
+                }
+            }
+            else if (order == 'd')
+            {
+
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
