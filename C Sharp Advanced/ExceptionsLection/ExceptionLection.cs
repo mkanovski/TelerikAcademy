aa46bc0b0980c5ef8aa4d1017888b9a56c290c26
Martin Kanovski <mkanovski@gmail.com>
2016-12-01 02:20:37 +0200

    CSS Presentation homework

---
+++ C Sharp Advanced/ExceptionsLection/ExceptionLection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExceptionsLection
+{
+    class ExceptionLection
+    {
+        static void Main()
+        {
+
+            try
+            {
+                int age = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException) // Тук се дефинира какъв ексепшън искаме да хванем. Без дефиниция хваща всичко
+            {
+
+                Console.WriteLine("Invalid age");
+            }
+        }
+    }
+}
---
+++ C Sharp Advanced/WorkingWithFilesLection/WorkingWithFilesLection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFilesLection
+{
+    class WorkingWithFilesLection
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("TODO-List.txt");
+
+            while (!reader.EndOfStream)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+
+            reader.Close();
+
+            // StringReader Stream
+
+            StringReader stringRead = new StringReader("string");
+
+            // Работа със файлове
+
+            var content = File.ReadAllText("TODO-List.text");  // Това има много класове за използване на файлове
+
+            // WriteStream
+
+            StreamWriter writer = new StreamWriter("TODO-List1.text");
+        }
+    }
+}
